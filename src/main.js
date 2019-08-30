9d816b69bb8320e8f02457856c8da401f7edb757
GBShadow <gbs.shadow@gmail.com>
2019-08-30 18:27:19 -0300

    Add feature Loading and error span

--- src/main.js
+++ src/main.js
@@ -14,6 +14,18 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true){
+    if(loading === true){
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando...'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    }else{
+      document.getElementById('loading').remove();
+    }
+  }
+
   async addRepository(event) {
     event.preventDefault();
 
@@ -21,22 +33,33 @@ class App {
 
     if (repoInput === 0)
       return;
-    
-    const response = await api.get(`/repos/${repoInput}`);
 
-    const { name, description, html_url, owner:{avatar_url}} = response.data;
+      this.setLoading();
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url,
-    });
+    try {
 
-    this.render();
+      const response = await api.get(`/repos/${repoInput}`);
+
+      const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url,
+      });
+
+      this.inputEl.value = '';
+
+      this.render();
+    } catch (error) {
+      alert('Repositorio não existe!');
+    }
+
+    this.setLoading(false);
   }
 
-  render(){
+  render() {
     this.listEl.innerHTML = '';
 
     this.repositories.forEach(repo => {
