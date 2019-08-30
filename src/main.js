a0a67d8e8693560d21219840a4f772780c5f7c6d
GBShadow <gbs.shadow@gmail.com>
2019-08-30 11:49:56 -0300

    Create add Repository static

---
+++ src/main.js
@@ -0,0 +1,27 @@
+class App {
+  constructor() {
+    this.repositories = [];
+
+    this.formEl = document.getElementById('repo-form');
+
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+
+  addRepository(event) {
+    event.preventDefault();
+    this.repositories.push({
+      name: 'rockeatseat.com.br',
+      description: 'Tire a sua ideia do papel e crie a sua startup.',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+      html_url: 'https://github.com/Rocketseat/',
+    });
+
+    console.log(this.repositories);
+  }
+}
+
+new App;
